e0b2eed4fd376353d883eb1cbc828481d43f46c6
Orcrax <matthieu.rebate@gmail.com>
2023-01-05 11:00:56 +0100

    +user admin + loggin

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('$2y$13$nG/WQp3G7PIk8RkRzJHlLOIf278QULfkNAiTSmJ6CU8eZIXYIggcK');
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
