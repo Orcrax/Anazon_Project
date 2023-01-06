fd9727376f96d8b5834c985fc781c3d2fbea68d7
Orcrax <matthieu.rebate@gmail.com>
2023-01-06 16:13:06 +0100

    twig + currency

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -13,16 +13,17 @@ class HelloController extends AbstractController
     #[Route('/hello/{name<\w+>}', methods: ['GET'], name: 'app_hello')]
     public function index(Request $request, string $name = 'remi'): Response
     {
-
         return $this->render('hello/index.html.twig', [
             'controller_name' => 'HelloController',
             'category' => [
                 'title' => 'Sample '.$name,
             ],
         ]);
-
-        return new JsonResponse([
-            'name' => $name,
-        ]);
     }
 }
+
+    //     return new JsonResponse([
+    //         'name' => $name,
+    //     ]);
+//     // }
+// }
